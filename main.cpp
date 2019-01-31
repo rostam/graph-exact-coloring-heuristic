efcff4867b3a479c57f3d32e5225ac0995c1022f
Ali Rostami <rostamiev@gmail.com>
2019-01-31 15:02:54 +0100

    writing tests for cycles

--- main.cpp
+++ main.cpp
@@ -4,37 +4,33 @@
 #include "datatypes.h"
 #include "Mtx2Graph.hpp"
 #include "generators/GeneralizedPeterson.h"
+#include "generators/Cycle.h"
 #include "exact_coloring/exact_coloring.h"
 
 using namespace std;
 
 int main() {
     Graph g = GeneralizedPeterson(5,2).generate();
-
-//    add_edge(0,1,g);
-//    add_edge(1,2,g);
-//    add_edge(2,0,g);
-//    add_edge(0,3,g);
-//    add_edge(1,3,g);
-//    add_edge(2,3,g);
-//    add_edge(3,4,g);
-//    add_edge(4,5,g);
-//    add_edge(4,6,g);
-//    add_edge(5,7,g);
-//    for(int i=7;i<10;i++) {
-//        add_edge(i,i+1,g);
-//    }
-    //matrix_market mm("ndtest.mtx");
-    //mm.MtxToBipGraph(g);
-
     cerr << "Number of vertices: " << num_vertices(g) << endl;
 
-    for(int i=1;i<10;i++) {
+    for(int i=2;i<10;i++) {
         int res =c_k(gen_ind_set_for_g(g),g,i);
-        //cerr << i << " " << res << " ";
         if(res!=0) {
-            cerr << "The number of color : " << i;
+            cerr << "The number of color for Generalized Peterson (5,2): " << i << endl;
             break;
+        }
+    }
+
+    for(int ii=3;ii <= 6;ii++) {
+        g = Cycle(ii).generate();
+        cerr << "Number of vertices: " << num_vertices(g) << endl;
+
+        for (int i = 2; i < 10; i++) {
+            int res = c_k(gen_ind_set_for_g(g), g, i);
+            if (res != 0) {
+                cerr << "The number of color for cycle " << i << ":" << endl;
+                break;
+            }
         }
     }
 }
